38eb45b24b29aeb808ee76404dd269c629f14c7e
erin.major <erinmajor254@gmail.com>
2026-03-20 21:59:09 -0600

    Real fix for CORs

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const authorRoutes = require('./routes/authors');
 const bookRoutes = require('./routes/books');
 
 app.use(bodyParser.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
 });
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', require('./routes'));
 app.use('/authors', authorRoutes);
 app.use('/books', bookRoutes);
