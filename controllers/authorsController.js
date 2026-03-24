59535813b9a2bbc2bce8192ea0cfb8335b69934c
erin.major <erinmajor254@gmail.com>
2026-03-23 18:53:46 -0600

    authors controller validation

--- controllers/authorsController.js
+++ controllers/authorsController.js
@@ -1,39 +1,51 @@
 const mongodb = require('../database/connect');
 const ObjectId = require('mongodb').ObjectId;
+const createError = require('http-errors');
 
 /* ****************************************
  *  Deliver all authors data
  * *************************************** */
-async function getAllAuthors(req, res) {
+async function getAllAuthors(req, res, next) {
   // #swagger.summary = 'Get all authors'
   // #swagger.description = 'Endpoint to get all authors from the database.'
   // #swagger.tags = ['Authors']
-  const results = await mongodb.getDb().db().collection('authors').find();
-  results.toArray().then((authors) => {
+  try {
+    const results = await mongodb.getDb().db().collection('authors').find();
+
+    const authors = await results.toArray();
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(authors);
-  });
+  } catch (error) {
+    next(error);
+  }
 }
 
 /* ****************************************
  *  Deliver author by ID data
  * *************************************** */
-async function getAuthorById(req, res) {
+async function getAuthorById(req, res, next) {
   // #swagger.summary = 'Get author by ID'
   // #swagger.description = 'Endpoint to get an author by ID from the database.'
   // #swagger.tags = ['Authors']
-  const authorId = new ObjectId(req.params.id);
-  const results = await mongodb.getDb().db().collection('authors').find({ _id: authorId });
-  results.toArray().then((authors) => {
+  try {
+    const authorId = new ObjectId(req.params.id);
+    const results = await mongodb.getDb().db().collection('authors').find({ _id: authorId });
+
+    const authors = await results.toArray();
+    if (authors.length === 0) {
+      return next(createError(404, 'Author not found'));
+    }
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(authors[0]);
-  });
+  } catch (error) {
+    next(error);
+  }
 }
 
 /* ****************************************
  *  Add an author
  * *************************************** */
-async function createAuthor(req, res) {
+async function createAuthor(req, res, next) {
   // #swagger.summary = 'Create a new author'
   // #swagger.description = 'Endpoint to create a new author in the database.'
   // #swagger.tags = ['Authors']
@@ -49,24 +61,28 @@ async function createAuthor(req, res) {
         }
       }
   */
-  const author = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    birthYear: req.body.birthYear,
-    bio: req.body.bio
-  };
-  const result = await mongodb.getDb().db().collection('authors').insertOne(author);
-  if (result.acknowledged) {
-    res.status(201).json(result.insertedId);
-  } else {
-    res.status(500).json(result.error || 'An error occurred while creating the author.');
+  try {
+    const author = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      birthYear: req.body.birthYear,
+      bio: req.body.bio
+    };
+    const result = await mongodb.getDb().db().collection('authors').insertOne(author);
+    if (result.acknowledged) {
+      res.status(201).json(result.insertedId);
+    } else {
+      res.status(500).json(result.error || 'An error occurred while creating the author.');
+    }
+  } catch (error) {
+    next(error);
   }
 }
 
 /* ****************************************
  *  Update an author
  * *************************************** */
-async function updateAuthor(req, res) {
+async function updateAuthor(req, res, next) {
   // #swagger.summary = 'Update an author by ID'
   // #swagger.description = 'Endpoint to update an existing author by its ID.'
   // #swagger.tags = ['Authors']
@@ -82,48 +98,56 @@ async function updateAuthor(req, res) {
         }
       }
   */
-  const authorId = new ObjectId(req.params.id);
-  const author = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    birthYear: req.body.birthYear,
-    bio: req.body.bio
-  };
-  const result = await mongodb
-    .getDb()
-    .db()
-    .collection('authors')
-    .updateOne(
-      { _id: authorId },
-      {
-        $set: {
-          firstName: author.firstName,
-          lastName: author.lastName,
-          birthYear: author.birthYear,
-          bio: author.bio
+  try {
+    const authorId = new ObjectId(req.params.id);
+    const author = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      birthYear: req.body.birthYear,
+      bio: req.body.bio
+    };
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection('authors')
+      .updateOne(
+        { _id: authorId },
+        {
+          $set: {
+            firstName: author.firstName,
+            lastName: author.lastName,
+            birthYear: author.birthYear,
+            bio: author.bio
+          }
         }
-      }
-    );
-  if (result.modifiedCount === 1) {
-    res.status(200).send();
-  } else {
-    res.status(500).json(result.error || 'An error occurred while updating the author.');
+      );
+    if (result.matchedCount === 0) {
+      next(createError(404, 'Author not found'));
+    } else {
+      res.status(200).send();
+    }
+  } catch (error) {
+    next(error);
   }
 }
 
 /* ****************************************
  *  Delete author data by id
  * *************************************** */
-async function deleteAuthor(req, res) {
+async function deleteAuthor(req, res, next) {
   // #swagger.summary = 'Delete an author by ID'
   // #swagger.description = 'Endpoint to delete an author from the database by its ID.'
   // #swagger.tags = ['Authors']
-  const authorId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db().collection('authors').deleteOne({ _id: authorId });
-  if (result.deletedCount === 1) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(result.error || 'An error occurred while deleting the author.');
+  try {
+    const authorId = new ObjectId(req.params.id);
+    const result = await mongodb.getDb().db().collection('authors').deleteOne({ _id: authorId });
+    if (result.deletedCount === 1) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(result.error || 'An error occurred while deleting the author.');
+    }
+  } catch (error) {
+    next(error);
   }
 }
 
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const mongodb = require('./database/connect');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const createError = require('http-errors');
 const port = process.env.PORT || 3000;
 const authorRoutes = require('./routes/authors');
 const bookRoutes = require('./routes/books');
@@ -19,6 +20,23 @@ app.use((req, res, next) => {
 app.use('/', require('./routes'));
 app.use('/authors', authorRoutes);
 app.use('/books', bookRoutes);
+
+// Error handling for 404 Not Found
+app.use((req, res, next) => {
+  next(createError(404, 'Not Found'));
+});
+
+// Error-handling middleware
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message
+    }
+  });
+});
 
 mongodb.initDb((err) => {
   if (err) {
